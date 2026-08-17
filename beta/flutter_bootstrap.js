1f4071b54657c0589038e64a21a82f31babcfbe0
devgaur477 <devgaur477@users.noreply.github.com>
2026-08-17 19:50:07 +0000

    Automated deployment of beta from Flutter app

--- beta/flutter_bootstrap.js
+++ beta/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3672792659" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "845891479" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
